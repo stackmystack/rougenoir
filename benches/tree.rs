ea4ad528fe66ddeaf1caef1eca35c2fb03774b35
Firas al-Khalil <firasalkhalil@gmail.com>
2025-04-11 09:20:23 +0200

    bench: add cached_tree

--- benches/tree.rs
+++ benches/tree.rs
@@ -23,6 +23,15 @@ fn insert_rougenoir(range: Range<usize>, tree: &mut rougenoir::Tree<usize, (), N
     }
 }
 
+fn insert_rougenoir_cached(
+    range: Range<usize>,
+    tree: &mut rougenoir::CachedTree<usize, (), Noop<usize, ()>>,
+) {
+    for k in range {
+        tree.insert(k, ());
+    }
+}
+
 fn bench_insert(c: &mut Criterion) {
     let mut group = c.benchmark_group("insert");
 
@@ -39,6 +48,14 @@ fn bench_insert(c: &mut Criterion) {
             let mut tree = rougenoir::Tree::<usize, (), Noop<usize, ()>>::new();
             b.iter(|| insert_rougenoir(black_box(0..size), black_box(&mut tree)));
         });
+        group.bench_with_input(
+            BenchmarkId::new("rougenoir::cached_tree", size),
+            &size,
+            |b, &size| {
+                let mut tree = rougenoir::CachedTree::<usize, (), Noop<usize, ()>>::new();
+                b.iter(|| insert_rougenoir_cached(black_box(0..size), black_box(&mut tree)));
+            },
+        );
     }
     group.finish();
 }
